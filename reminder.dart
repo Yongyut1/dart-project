ce16eb0ae8ba8647ea11eb1dffb8ac1eae7797c8
Yongyut1 <wavekung1470@gmail.com>
2024-11-18 15:43:17 +0700

    first commit

---
+++ reminder.dart
@@ -0,0 +1,15 @@
+void main() {
+
+int num1 = 10; 
+int num2 = 3; 
+ 
+
+  
+int sum1 = num1~/num2;
+int sum2 = num1%num2;
+    
+
+print("quotient = $sum1");  
+print("remainder = $sum2");  
+   
+}
